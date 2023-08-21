2d2ba12721e4b553dfe1dbbb1691d767090df7ba
saiharshavardhan-1998 <saiharsha1398@gmail.com>
2023-08-21 05:30:42 +0000

    Super-Over-League

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  <div className="container">
+    <h1>Super Over League</h1>
+
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="images"
+      ></img>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="images"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
